a6c73d233f61e47ded60d3f44328878ea1b947d2
krishna <krish.roy92@gmail.com>
2025-01-16 00:01:43 +0530

    added new

--- codefiles/qustion_answering.py
+++ codefiles/qustion_answering.py
@@ -1,13 +1,23 @@
 from transformers import pipeline
 import os
 
-
+question_answering = pipeline(task='visual-question-answering', model='dandelin/vilt-b32-finetuned-vqa')
+dqa= pipeline(task='document-question-answering',model ='impira/layoutlm-document-qac')
 def visual_answer_question(question, context):
-    question_answering = pipeline(task='visual-question-answering', model='dandelin/vilt-b32-finetuned-vqa')
+
     output = question_answering(question=question, image=context)
     for i in output:
         print(i[0]['answer'])
 
+def doc_question_answer(question, context):
+    results = dqa(image= context, question=question)
+    print(results)
+
+
 
 if __name__ == "__main__":
-    visual_answer_question(["What is the color of the fruit?","what is this fruit"], "../test-images/IMG_TEST_1.jpg")
+    all_files = os.listdir('../test-pdfs')
+
+    for file in all_files:
+        #visual_answer_question(["What is the color of the fruit?","what is this fruit"], "../test-images/"+file)
+        doc_question_answer('What is this document?',context="../test-pdfs/"+file)
